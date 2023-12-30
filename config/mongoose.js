87f2a6c3f6af46fd61ee3744dd514ef2b9201ea5
Nilesh-Patil0310 <patilnilesh093@gmail.com>
2023-12-30 12:49:35 +0530

    create all required pages,controller and routes for them

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 async function db(){
-    await mongoose.connect('mongodb+srv://ernileshpatil098:rQHVVwUaZS7BPi1Y@cluster0.omo5kfv.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://ernileshpatil098:rQHVVwUaZS7BPi1Y@cluster0.omo5kfv.mongodb.net/placement-cell?retryWrites=true&w=majority');
     console.log("connection Successfull !! ");
 }
 db().catch(error =>console.log("connection not successfull !!"));
