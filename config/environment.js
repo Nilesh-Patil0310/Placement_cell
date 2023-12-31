89449f9e7f50be58278937df15ea2c7ec9db0ef7
Nilesh-Patil0310 <146980321+Nilesh-Patil0310@users.noreply.github.com>
2023-12-31 23:10:43 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -8,7 +8,7 @@ const developement = {
   secret_key:
     "#placement_Cell@careercamp$$student&&&interface##Nilesh^^Patil%%",
   mongoose_path:
-    "mongodb://127.0.0.1:27017/placement_cell_db",
+    "mongodb+srv://ernileshpatil098:rQHVVwUaZS7BPi1Y@cluster0.omo5kfv.mongodb.net/placement-cell?retryWrites=true&w=majority",
   api_path: "https://remotive.com/api/remote-jobs",
 };
 module.exports = developement;
