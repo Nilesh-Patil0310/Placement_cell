54dc686878259c953e163cc75a138e1db9e9aa55
Nilesh-Patil0310 <patilnilesh093@gmail.com>
2023-12-31 20:57:02 +0530

    change database setting

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function db() {
   mongoose.set("strictQuery", true);
   await mongoose.connect(
-    "mongodb+srv://ernileshpatil098:rQHVVwUaZS7BPi1Y@cluster0.omo5kfv.mongodb.net/placement-cell?retryWrites=true&w=majority"
+    "mongodb+srv://rQHVVwUaZS7BPi1Y@cluster0.omo5kfv.mongodb.net/placement-cell-prod?retryWrites=true&w=majority"
   );
   console.log("connection Successfull !! ");
 }
